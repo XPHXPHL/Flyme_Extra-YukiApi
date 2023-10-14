73db2b68ef39bbbceedb567489bc8f233551f807
XiaoPoHai <2262519065@qq.com>
2023-10-14 17:04:32 +0800

    add BackVibrator

---
+++ app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/systemUI/vibrator/back/BackVibrator.kt
@@ -0,0 +1,24 @@
+package com.xlp.flyme.extra.hook.modules.systemUI.vibrator.back
+
+import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.log.YLog
+import com.highcapable.yukihookapi.hook.log.YLog.Configs.tag
+import com.xlp.flyme.extra.utils.XUtils
+
+object BackVibrator : YukiBaseHooker() {
+    override fun onHook() {
+        "com.flyme.systemui.navigationbar.gestural.EdgeBackView".toClass()
+            .method {
+                name = "triggerBack"
+            }.hook {
+                after {
+                    XUtils.vibratorHelper(31021)
+                }
+            }.result {
+                onAllFailure {
+                    YLog.error(msg = "${it.message}", tag = tag)
+                }
+            }
+    }
+}
