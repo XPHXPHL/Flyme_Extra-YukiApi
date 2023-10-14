39b4b6504104617f1dad2e0317663a57989ae92b
XiaoPoHai <2262519065@qq.com>
2023-10-14 17:04:39 +0800

    add FaceVibrator

---
+++ app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/systemUI/vibrator/face/FaceVibrator.kt
@@ -0,0 +1,24 @@
+package com.xlp.flyme.extra.hook.modules.systemUI.vibrator.face
+
+import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.log.YLog
+import com.highcapable.yukihookapi.hook.log.YLog.Configs.tag
+import com.xlp.flyme.extra.utils.XUtils
+
+object FaceVibrator : YukiBaseHooker() {
+    override fun onHook() {
+        "com.flyme.systemui.facerecognition.FaceRecognitionAnimationView".toClass()
+            .method {
+                name = "startSuccessAnimation"
+            }.hook {
+                after {
+                    XUtils.vibratorHelper(31021)
+                }
+            }.result {
+                onAllFailure {
+                    YLog.error(msg = "${it.message}", tag = tag)
+                }
+            }
+    }
+}
