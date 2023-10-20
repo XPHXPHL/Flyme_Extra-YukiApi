3133a4b990cf907f0f74be1a437431c678d57f36
XiaoPoHai <2262519065@qq.com>
2023-10-20 23:24:47 +0800

    add MainPage

---
+++ app/src/main/kotlin/com/xlp/flyme/extra/activity/pages/MainPage.kt
@@ -0,0 +1,10 @@
+package com.xlp.flyme.extra.activity.pages
+
+import cn.fkj233.ui.activity.annotation.BMMainPage
+import cn.fkj233.ui.activity.data.BasePage
+
+@BMMainPage("Flyme Extra")
+class MainPage : BasePage() {
+    override fun onCreate() {
+    }
+}
