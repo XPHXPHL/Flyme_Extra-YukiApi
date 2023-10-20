aed41c762e7ca20162f9dd3d0fcb54fb16a596d8
XiaoPoHai <2262519065@qq.com>
2023-10-20 23:24:07 +0800

    XUtils:add exec function

--- app/src/main/kotlin/com/xlp/flyme/extra/utils/XUtils.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/utils/XUtils.kt
@@ -5,6 +5,7 @@ import android.app.AndroidAppHelper
 import android.app.Service
 import android.os.VibrationEffect
 import android.os.Vibrator
+import java.io.DataOutputStream
 
 @SuppressLint("WrongConstant", "NewApi")
 object XUtils {
@@ -13,4 +14,28 @@ object XUtils {
             AndroidAppHelper.currentApplication().applicationContext.getSystemService(Service.VIBRATOR_SERVICE) as Vibrator
         mVibrator.vibrate(VibrationEffect.createPredefined(effectId))
     }
+
+    fun execShell(command: String) {
+        try {
+            val p = Runtime.getRuntime().exec("su")
+            val outputStream = p.outputStream
+            val dataOutputStream = DataOutputStream(outputStream)
+            dataOutputStream.writeBytes(command)
+            dataOutputStream.flush()
+            dataOutputStream.close()
+            outputStream.close()
+        } catch (t: Throwable) {
+            t.printStackTrace()
+        }
+    }
+
+    fun execShell(commands: Array<String>): String {
+        val stringBuilder = java.lang.StringBuilder()
+        for (command in commands) {
+            stringBuilder.append(execShell(command))
+            stringBuilder.append("\n")
+        }
+        return stringBuilder.toString()
+    }
+
 }
