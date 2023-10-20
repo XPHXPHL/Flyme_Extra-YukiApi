2eadc4c14a685381090df4dc58f0a6dd11eeea5a
XiaoPoHai <2262519065@qq.com>
2023-10-20 23:24:22 +0800

    add MainActivity

---
+++ app/src/main/kotlin/com/xlp/flyme/extra/activity/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.xlp.flyme.extra.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import cn.fkj233.ui.activity.MIUIActivity
+import cn.fkj233.ui.dialog.MIUIDialog
+import com.highcapable.yukihookapi.YukiHookAPI
+import com.xlp.flyme.extra.R
+import com.xlp.flyme.extra.activity.pages.MainPage
+import com.xlp.flyme.extra.activity.pages.MenuPage
+import com.xlp.flyme.extra.utils.BackupUtils
+import kotlin.system.exitProcess
+
+
+class MainActivity : MIUIActivity() {
+    @SuppressLint("SdCardPath")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        checkLSPosed()
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun checkLSPosed() {
+        setSP(getSharedPreferences("Flyme-Extra_Config", MODE_WORLD_READABLE))
+        if (!YukiHookAPI.Status.isModuleActive) {
+            MIUIDialog(this) {
+                setTitle(R.string.tips)
+                setMessage(R.string.not_support)
+                setCancelable(false)
+                setRButton(R.string.done) {
+                    exitProcess(0)
+                }
+            }.show()
+        }
+    }
+
+    init {
+        activity = this
+        registerPage(MenuPage::class.java)
+        registerPage(MainPage::class.java)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (data != null && resultCode == RESULT_OK) {
+            when (requestCode) {
+                BackupUtils.CREATE_DOCUMENT_CODE -> {
+                    BackupUtils.handleCreateDocument(activity, data.data)
+                }
+
+                BackupUtils.OPEN_DOCUMENT_CODE -> {
+                    BackupUtils.handleReadDocument(activity, data.data)
+                }
+
+            }
+        }
+    }
+
+}
