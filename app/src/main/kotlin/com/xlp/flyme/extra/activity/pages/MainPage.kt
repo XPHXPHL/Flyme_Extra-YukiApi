f5f974a189b40125add0d924dd9f035abd1f4cff
小破孩呐 <145167297+XiaoPoHaiLwz@users.noreply.github.com>
2023-10-27 20:23:32 +0800

    添加VibratorPage的入口

--- app/src/main/kotlin/com/xlp/flyme/extra/activity/pages/MainPage.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/activity/pages/MainPage.kt
@@ -2,9 +2,19 @@ package com.xlp.flyme.extra.activity.pages
 
 import cn.fkj233.ui.activity.annotation.BMMainPage
 import cn.fkj233.ui.activity.data.BasePage
+import cn.fkj233.ui.activity.view.TextSummaryV
+import com.xlp.flyme.extra.R
 
 @BMMainPage("Flyme Extra")
 class MainPage : BasePage() {
     override fun onCreate() {
+        Page(
+            this.getDrawable(R.drawable.ic_launcher_background),
+            TextSummaryV(textId = R.string.vibrator_page),
+            round = 8f,
+            onClickListener = {
+                showFragment("VibratorPage")
+            }
+        )
     }
 }
