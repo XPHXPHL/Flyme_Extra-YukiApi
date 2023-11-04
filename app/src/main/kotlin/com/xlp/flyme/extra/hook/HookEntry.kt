79e58270dfe9b5aeb69af0811da0e9199064e142
XiaoPoHai <2262519065@qq.com>
2023-11-04 15:17:08 +0800

    HookEntry:加载Hooker 1.AppShade 2.LockGameFps 3.ForcedScreenCapture 4.ForcedScreenCapture2

--- app/src/main/kotlin/com/xlp/flyme/extra/hook/HookEntry.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/hook/HookEntry.kt
@@ -7,10 +7,14 @@ import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.log.YLog
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 import com.xlp.flyme.extra.BuildConfig
+import com.xlp.flyme.extra.hook.modules.android.forcedScreenCapture.ForcedScreenCapture
+import com.xlp.flyme.extra.hook.modules.android.gameFps.LockGameFps
+import com.xlp.flyme.extra.hook.modules.systemUI.appShade.AppShade
 import com.xlp.flyme.extra.hook.modules.systemUI.blur.SupportBlur
 import com.xlp.flyme.extra.hook.modules.systemUI.vibrator.back.BackVibrator
 import com.xlp.flyme.extra.hook.modules.systemUI.vibrator.face.FaceVibrator
 import com.xlp.flyme.extra.hook.modules.systemUI.vibrator.fingerprint.FingerprintVibrator
+import com.xlp.flyme.extra.hook.modules.systemUIex.forcedScreenCapture.ForcedScreenCapture2
 
 @InjectYukiHookWithXposed
 class HookEntry : IYukiHookXposedInit {
@@ -31,6 +35,14 @@ class HookEntry : IYukiHookXposedInit {
             loadHooker(BackVibrator)
             loadHooker(FaceVibrator)
             loadHooker(FingerprintVibrator)
+            loadHooker(AppShade)
+        }
+        loadApp(name = "com.flyme.systemuiex"){
+            loadHooker(ForcedScreenCapture2)
+        }
+        loadApp(name = "android") {
+            loadHooker(LockGameFps)
+            loadHooker(ForcedScreenCapture)
         }
         YLog.saveToFile("/sdcard/Documents/YukiApi-Flyme_Extra-Debug_Log.log")
     }
