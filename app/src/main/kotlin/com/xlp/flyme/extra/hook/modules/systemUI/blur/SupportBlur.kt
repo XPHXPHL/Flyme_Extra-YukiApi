81a7e61b32dfd6492ad20f22c9f6a074d2e85a65
XiaoPoHai <2262519065@qq.com>
2023-11-04 15:18:55 +0800

    Add switch for Hooker that isSupportBlur

--- app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/systemUI/blur/SupportBlur.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/systemUI/blur/SupportBlur.kt
@@ -4,28 +4,31 @@ import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.log.YLog
 import com.highcapable.yukihookapi.hook.log.YLog.Configs.tag
+import com.xlp.flyme.extra.utils.getBoolean
 
 object SupportBlur : YukiBaseHooker() {
     override fun onHook() {
-        fun isSupportBlur(clazz: Class<*>) {
-            clazz
-                .method {
-                    name = "isSupportBlur"
-                }.hook {
-                    replaceToTrue()
-                }.onAllFailure {
-                    YLog.error(tag = tag, e = it)
-                }
-        }
+        if (getBoolean("blur_enabled", false)) {
+            fun isSupportBlur(clazz: Class<*>) {
+                clazz
+                    .method {
+                        name = "isSupportBlur"
+                    }.hook {
+                        replaceToTrue()
+                    }.onAllFailure {
+                        YLog.error(tag = tag, e = it)
+                    }
+            }
 
-        val targetClass =
-            "com.android.systemui.statusbar.notification.row.ActivatableNotificationView".toClass()
-        val targetClass2 =
-            "com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout".toClass()
-        val targetClass3 =
-            "com.android.systemui.media.MediaCarouseTransitionLayout".toClass()
-        isSupportBlur(targetClass)
-        isSupportBlur(targetClass2)
-        isSupportBlur(targetClass3)
+            val targetClass =
+                "com.android.systemui.statusbar.notification.row.ActivatableNotificationView".toClass()
+            val targetClass2 =
+                "com.android.systemui.statusbar.notification.stack.NotificationStackScrollLayout".toClass()
+            val targetClass3 =
+                "com.android.systemui.media.MediaCarouseTransitionLayout".toClass()
+            isSupportBlur(targetClass)
+            isSupportBlur(targetClass2)
+            isSupportBlur(targetClass3)
+        }
     }
 }
