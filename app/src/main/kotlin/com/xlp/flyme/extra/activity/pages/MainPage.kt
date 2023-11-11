71d1ce01e9802421078dd0e948593e606892e136
XiaoPoHai <2262519065@qq.com>
2023-11-11 08:35:57 +0800

    Change MainPage style

--- app/src/main/kotlin/com/xlp/flyme/extra/activity/pages/MainPage.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/activity/pages/MainPage.kt
@@ -8,22 +8,30 @@ import com.xlp.flyme.extra.R
 @BMMainPage("Flyme Extra")
 class MainPage : BasePage() {
     override fun onCreate() {
-        Page(
-            this.getDrawable(R.drawable.ic_launcher_background),
-            TextSummaryV(textId = R.string.vibrator_page),
-            round = 8f,
-            onClickListener = {
+        TextSummaryWithArrow(
+            TextSummaryV(textId = R.string.android_page, tipsId = R.string.android_page_summary ,onClickListener = {
+                showFragment("AndroidPage")
+            }
+            )
+        )
+        Line()
+        TextSummaryWithArrow(
+            TextSummaryV(textId = R.string.vibrator_page, onClickListener = {
                 showFragment("VibratorPage")
             }
+            )
         )
-
-        Page(
-            this.getDrawable(R.drawable.ic_launcher_background),
-            TextSummaryV(textId = R.string.systemui_page),
-            round = 8f,
-            onClickListener = {
+        TextSummaryWithArrow(
+            TextSummaryV(textId = R.string.settings_page, onClickListener = {
+                showFragment("SettingsPage")
+            }
+            )
+        )
+        TextSummaryWithArrow(
+            TextSummaryV(textId = R.string.systemui_page, onClickListener = {
                 showFragment("SystemUiPage")
             }
+            )
         )
     }
 }
