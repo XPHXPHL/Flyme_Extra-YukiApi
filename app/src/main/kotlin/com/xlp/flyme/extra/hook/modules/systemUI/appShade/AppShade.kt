ae02829bcde529472a33b660ee6d41db156112ab
XiaoPoHai <2262519065@qq.com>
2023-11-11 08:33:42 +0800

    AppShade:add onAllFailure and log input

--- app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/systemUI/appShade/AppShade.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/systemUI/appShade/AppShade.kt
@@ -3,6 +3,8 @@ package com.xlp.flyme.extra.hook.modules.systemUI.appShade
 import android.os.IBinder
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.log.YLog
+import com.highcapable.yukihookapi.hook.log.YLog.Configs.tag
 import com.xlp.flyme.extra.utils.getBoolean
 import de.robv.android.xposed.XposedHelpers
 
@@ -28,6 +30,8 @@ object AppShade : YukiBaseHooker() {
                             0
                         )
                     }
+                }.onAllFailure {
+                    YLog.error(tag = tag, e = it)
                 }
         }
     }
