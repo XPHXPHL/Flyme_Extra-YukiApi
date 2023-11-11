6c109614434a8245cdab8f01561cc04dc942ad9d
XiaoPoHai <2262519065@qq.com>
2023-11-11 08:37:09 +0800

    Update HookEntry 1.UnlockGameFps 2.ForcedScreenCapture 3.ClipboardEditor 4.PressUnlock 5.CipherDiskVibrator

--- app/src/main/kotlin/com/xlp/flyme/extra/hook/HookEntry.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/hook/HookEntry.kt
@@ -8,9 +8,12 @@ import com.highcapable.yukihookapi.hook.log.YLog
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 import com.xlp.flyme.extra.BuildConfig
 import com.xlp.flyme.extra.hook.modules.android.forcedScreenCapture.ForcedScreenCapture
-import com.xlp.flyme.extra.hook.modules.android.gameFps.LockGameFps
+import com.xlp.flyme.extra.hook.modules.android.unlockGameFps.UnlockGameFps
+import com.xlp.flyme.extra.hook.modules.settings.cipherDiskVibrator.CipherDiskVibrator
+import com.xlp.flyme.extra.hook.modules.settings.pressUnlock.PressUnlock
 import com.xlp.flyme.extra.hook.modules.systemUI.appShade.AppShade
 import com.xlp.flyme.extra.hook.modules.systemUI.blur.SupportBlur
+import com.xlp.flyme.extra.hook.modules.systemUI.clipboardEditor.ClipboardEditor
 import com.xlp.flyme.extra.hook.modules.systemUI.vibrator.back.BackVibrator
 import com.xlp.flyme.extra.hook.modules.systemUI.vibrator.face.FaceVibrator
 import com.xlp.flyme.extra.hook.modules.systemUI.vibrator.fingerprint.FingerprintVibrator
@@ -30,19 +33,24 @@ class HookEntry : IYukiHookXposedInit {
 
     @SuppressLint("SdCardPath")
     override fun onHook() = encase {
+        loadApp(name = "android") {
+            loadHooker(UnlockGameFps)
+            loadHooker(ForcedScreenCapture)
+        }
         loadApp(name = "com.android.systemui") {
             loadHooker(SupportBlur)
             loadHooker(BackVibrator)
             loadHooker(FaceVibrator)
             loadHooker(FingerprintVibrator)
             loadHooker(AppShade)
+            loadHooker(ClipboardEditor)
+            loadHooker(PressUnlock)
+        }
+        loadApp(name = "com.android.settings") {
+            loadHooker(CipherDiskVibrator)
         }
         loadApp(name = "com.flyme.systemuiex") {
             loadHooker(ForcedScreenCapture2)
-        }
-        loadApp(name = "android") {
-            loadHooker(LockGameFps)
-            loadHooker(ForcedScreenCapture)
         }
         YLog.saveToFile("/sdcard/Documents/YukiApi-Flyme_Extra-Debug_Log.log")
     }
