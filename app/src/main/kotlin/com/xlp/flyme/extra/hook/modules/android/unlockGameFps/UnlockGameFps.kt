47e3cb3badcfedd2f9489431cafa7faae5c19b57
XiaoPoHai <2262519065@qq.com>
2023-11-11 08:33:00 +0800

    Rename: LockGameFps -> UnlockGameFps

--- app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/android/unlockGameFps/UnlockGameFps.kt
+++ app/src/main/kotlin/com/xlp/flyme/extra/hook/modules/android/unlockGameFps/UnlockGameFps.kt
@@ -1,4 +1,4 @@
-package com.xlp.flyme.extra.hook.modules.android.gameFps
+package com.xlp.flyme.extra.hook.modules.android.unlockGameFps
 
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
@@ -6,9 +6,9 @@ import com.highcapable.yukihookapi.hook.log.YLog
 import com.highcapable.yukihookapi.hook.log.YLog.Configs.tag
 import com.xlp.flyme.extra.utils.getBoolean
 
-object LockGameFps : YukiBaseHooker() {
+object UnlockGameFps : YukiBaseHooker() {
     override fun onHook() {
-        if (getBoolean("game_fps", false)) {
+        if (getBoolean("unlock_game_fps", false)) {
             "com.android.server.wm.WindowState".toClass()
                 .method {
                     name = "isFocused"
